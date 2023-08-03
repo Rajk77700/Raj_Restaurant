716d8e1354fc4e5441539afb816104d881fe9733
Rajk77700 <140932836+Rajk77700@users.noreply.github.com>
2023-08-03 10:38:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Resturant from './Component/Basics/Resturant';
+
+const App = () => {
+  return (
+    <>
+    <h1>Raj Resturant</h1>
+    <h3>We Serves Quality Food Only</h3>
+      <Resturant/>
+    </>
+  )
+}
+
+export default App
